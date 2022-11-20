bd9fa8139db3b0377028698e2e0334464c8123dd
Sonu Sathyadas <githubtraining@wibmo.com>
2022-11-20 12:30:44 +0530

    updated msg

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ByteStreamWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
